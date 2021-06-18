98efe7f3815aeafd525ad7eb4a5f1c38803b3159
Василий Олейник <goooneoooogle@gmail.com>
2021-06-18 11:15:58 +0300

    [ADD]: get axios data

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -4,7 +4,7 @@ const TheFooter = () => (
     <footer class="page-footer">
         <div class="footer-copyright">
             <div class="container">
-                © 2021 Разработка: <a class="grey-text text-lighten-4 right" href="https://teleg.run/vasiliy_oleinik">Василий Олейник</a>
+                © 2021 Разработка: <a class="grey-text text-lighten-4" href="https://teleg.run/vasiliy_oleinik">Василий Олейник</a>
                 <a class="grey-text text-lighten-4 right" href="https://github.com/VasiliiOleinik/merehead">GitHub</a>
             </div>
         </div>
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useCallback, useEffect } from 'react';
+import axios from 'axios';
 
 const Dashboard = () => {
+    const API_URL = process.env.REACT_APP_BACKEND_API;
+    const getUsers = useCallback(() => {
+        axios({
+            method: 'GET',
+            url: `${API_URL}users`,
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log('error',error);
+            });
+    }, [API_URL]);
+    useEffect(() => {
+        getUsers();
+    }, [getUsers]);
+
     return (
         <span>Dashboard</span>
     )
